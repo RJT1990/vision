0cabb5cedcc6743f58f7ed833f6bf6b978b9dfd4
Ross Taylor <rj-taylor@live.co.uk>
2019-08-23 20:08:26 +0100

    Create sotabench.py

---
+++ sotabench.py
@@ -0,0 +1,23 @@
+from torchvision.models.alexnet import alexnet
+import torchvision.transforms as transforms
+from sotabench.image_classification import ImageNet
+import PIL
+import torch
+
+# Define Transforms    
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+b0_input_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    normalize,
+])
+
+# Run Evaluation
+ImageNet.benchmark(
+    model=alexnet(pretrained=True),
+    paper_model_name='AlexNet',
+    input_transform=b0_input_transform,
+    batch_size=256,
+    num_gpu=1
+)
